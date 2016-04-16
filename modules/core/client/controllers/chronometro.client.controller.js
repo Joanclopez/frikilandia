b7bb7579c945281768e1b14533c165b545a92b32
joanclopez <joanclopezm@gmail.com>
2016-04-16 14:31:34 -0600

    change green

--- modules/core/client/controllers/chronometro.client.controller.js
+++ modules/core/client/controllers/chronometro.client.controller.js
@@ -6,45 +6,45 @@ angular.module('core').controller('ChronometroController', ['$scope','$state', '
     $scope.red=1;
     $scope.displayYellow=false;
     $scope.displayRed=false;
-
-
-    $scope.startTimer = function (){
-      $scope.$broadcast('timer-start');
-      $scope.timerRunning = true;
-    };
-
-    $scope.stopTimer = function (){
-      $scope.$broadcast('timer-stop');
-      $scope.timerRunning = false;
-    };
-
-    $scope.addTime=function(){
-      $scope.initChronometer=$scope.initChronometerInput*60;
-    }
-
-    $scope.addYellow=function(){
-      $scope.yellow=$scope.changeYellow*60;
-    }
-    $scope.addRed=function(){
-      $scope.red=$scope.changeRed*60;
-    }
-    $scope.$on('timer-tick', function (event, args) {
-    console.log(args);
-      $scope.saveTime=args.millis/1000;
-      $scope.changeMili()
-
-    });
-
-    $scope.changeMili=function(){
-      if ($scope.saveTime<$scope.yellow&&$scope.saveTime>=$scope.red) {
-        $scope.displayYellow=true;
-        console.log('entro a yello');
-      }else if($scope.saveTime<$scope.red ){
-        console.log('entro a red');
-        $scope.displayYellow=false;
-        $scope.displayRed=true;
-      }
-    };
+    // 
+    //
+    // $scope.startTimer = function (){
+    //   $scope.$broadcast('timer-start');
+    //   $scope.timerRunning = true;
+    // };
+    //
+    // $scope.stopTimer = function (){
+    //   $scope.$broadcast('timer-stop');
+    //   $scope.timerRunning = false;
+    // };
+    //
+    // $scope.addTime=function(){
+    //   $scope.initChronometer=$scope.initChronometerInput*60;
+    // };
+    //
+    // $scope.addYellow=function(){
+    //   $scope.yellow=$scope.changeYellow*60;
+    // };
+    // $scope.addRed=function(){
+    //   $scope.red=$scope.changeRed*60;
+    // };
+    // $scope.$on('timer-tick', function (event, args) {
+    // console.log(args);
+    //   $scope.saveTime=args.millis/1000;
+    //   $scope.changeMili();
+    //
+    // });
+    //
+    // $scope.changeMili=function(){
+    //   if ($scope.saveTime<$scope.yellow&&$scope.saveTime>=$scope.red) {
+    //     $scope.displayYellow=true;
+    //     console.log('entro a yello');
+    //   }else if($scope.saveTime<$scope.red ){
+    //     console.log('entro a red');
+    //     $scope.displayYellow=false;
+    //     $scope.displayRed=true;
+    //   }
+    // };
 
     $scope.displayGreenFunction=function(){
       $scope.displayYellow=false;
